4158eb8bac942b6d413bf23c265840d515d3af4d
Minna Iholin <ihqminna@helsinki.fi>
2020-05-19 11:36:11 +0300

    tiedostoja

--- application/__init__.py
+++ application/__init__.py
@@ -3,11 +3,13 @@ app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://reseptikirjasto.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///reseptikirjasto.db"
 app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
 from application import views
+
+from application.recipes import models
 
 db.create_all()
--- application/recipes/models.py
+++ application/recipes/models.py
@@ -1,4 +1,14 @@
 from application import db
 
-class Task(db.Model):
-	id = d.Column(db.Integer, primary
+class Recipe(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	nimi = db.Column(db.String, nullable=False)
+	annoksia = db.Column(db.Integer, nullable=False)
+	aika = db.Column(db.Integer, nullable=False)
+	ohje = db.Column(db.String, nullable=False)
+
+	def __init__(self, nimi, annoksia, aika, ohje):
+		self.nimi = nimi
+		self.annoksia = annoksia
+		self.aika = aika
+		self.ohje = ohje
