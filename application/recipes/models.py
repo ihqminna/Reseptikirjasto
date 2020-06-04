38aad8dc09f57a433b276e14a6a1d99f36124652
Minna Iholin <ihqminna@helsinki.fi>
2020-06-04 12:55:56 +0300

    resepti-ainesosa-yhteyden luomista

--- application/recipes/models.py
+++ application/recipes/models.py
@@ -2,7 +2,8 @@ from application import db
 from application.ingredients.models import Ingredient
 from application.measures.models import Measure
 
-recipe_ingredient = db.Table('recipe_ingredient', db.Column('recipe_id', db.Integer, db.ForeignKey('recipe.id'), primary_key=True), db.Column('ingredient_name', db.Integer, db.ForeignKey('ingredient.nimi'), primary_key=True), db.Column('measure_name', db.String, db.ForeignKey('measure.nimi')), db.Column('amount', db.Integer))
+ingredient_recipe = db.Table('ingredient_recipe', db.Column('recipe_id', db.Integer, db.ForeignKey('recipe.id')), db.Column('ingredient_name', db.Integer, db.ForeignKey('ingredient.nimi'), primary_key=True), db.Column('measure_name', db.String, db.ForeignKey('measure.nimi')))
+
 
 class Recipe(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -10,7 +11,6 @@ class Recipe(db.Model):
 	annoksia = db.Column(db.Integer, nullable=False)
 	aika = db.Column(db.Integer, nullable=False)
 	ohje = db.Column(db.String, nullable=False)
-	recipe_ingredient = db.relationship('Recipe_Ingredient', secondary=recipe_ingredient, lazy='subquery', backref=db.backref('ingredients', lazy=True))
 
 	def __init__(self, nimi, annoksia, aika, ohje):
 		self.nimi = nimi
